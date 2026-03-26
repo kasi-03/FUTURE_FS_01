4d90479eee307b5a57752cead112f48a4e46ee05
kasi-03 <kasiraoarepati@gmail.com>
2026-03-26 16:33:31 +0530

    change in java script

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
   if (menuBtn && navLinks) {
     menuBtn.addEventListener("click", () => {
       navLinks.classList.toggle("active");
+
       if (navLinks.classList.contains("active")) {
-        menuBtn.innerHTML = "&times;"; 
+        menuBtn.innerHTML = "&times;";
       } else {
-        menuBtn.innerHTML = "&#9776;"; 
+        menuBtn.innerHTML = "&#9776;";
       }
     });
+
+    document.querySelectorAll('.nav-links a').forEach(link => {
+      link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        menuBtn.innerHTML = "&#9776;";
+      });
+    });
   }
-});
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => {
-    navLinks.classList.remove('active');
-    menuBtn.innerHTML = "&#9776;";
-  });
 });
